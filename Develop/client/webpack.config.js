30efd3469b740e36003fbeea0c963f13a7c71b73
tedlopus <tedlopus@gmail.com>
2022-05-07 09:21:06 -0600

    started on webpack config and asset caching

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,48 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'ELTE Best Text Editor'
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'ELTE',
+        short_name: 'ELTE',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
-      ],
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              },
+            },
+          },
+        ],
     },
   };
 };
